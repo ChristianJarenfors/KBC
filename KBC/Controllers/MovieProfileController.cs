0d5561e4bc4a7671bff5c35d2f46ac66ecbbc7d1
Robert Hansson <roberthanssonr@gmail.com>
2016-04-25 16:58:22 +0200

    Gjort så att hyperlänkarna funkar till Serie-Profile och trycka på logo, så kommer man till första-sidan.

---
+++ KBC/Controllers/MovieProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KBC.Controllers
+{
+    public class MovieProfileController : Controller
+    {
+        // GET: MovieProfile
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
