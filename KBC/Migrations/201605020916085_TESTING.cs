ce8942466a3f8a52bdeb442c8ce2de33e2699930
Studerande <Studerande@hermods.local>
2016-05-02 11:21:24 +0200

    REEEEEEEKT

---
+++ KBC/Migrations/201605020916085_TESTING.cs
@@ -0,0 +1,38 @@
+namespace KBC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TESTING : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        Password = c.String(),
+                        Email = c.String(),
+                        ProfilePictureUrl = c.String(),
+                        Age = c.Int(nullable: false),
+                        Country = c.String(),
+                        Gender = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Series", "User_Id", c => c.Int());
+            CreateIndex("dbo.Series", "User_Id");
+            AddForeignKey("dbo.Series", "User_Id", "dbo.Users", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Series", "User_Id", "dbo.Users");
+            DropIndex("dbo.Series", new[] { "User_Id" });
+            DropColumn("dbo.Series", "User_Id");
+            DropTable("dbo.Users");
+        }
+    }
+}
