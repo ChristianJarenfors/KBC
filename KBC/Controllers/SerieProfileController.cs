9e8123e62eb108401b94f3b7b79967e1bcf852d1
ChrilleJ <christian.jarenfors@gmail.com>
2016-05-03 15:35:15 +0200

    Fixar till Serieprofilsidan

--- KBC/Controllers/SerieProfileController.cs
+++ KBC/Controllers/SerieProfileController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KBC.Models;
 
 namespace KBC.Controllers
 {
     public class SerieProfileController : Controller
     {
         // GET: MovieProfile
-        public ActionResult Index()
+        public ActionResult Index(int? id)
         {
-            return View();
+            SerieContext SC = new SerieContext();
+            if (id!=null)
+            {
+                
+                var Serie = SC.Serie.Where(s => s.SerieId == id).First();
+                return View(Serie);
+            }
+            return View(SC.Serie.First());
         }
     }
 }
